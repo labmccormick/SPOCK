590db2647146e4885b5acb9533c50878a8c3a032
Smaller <ericmsmall@gmail.com>
2020-05-24 17:28:06 -0600

    added underscore

--- R/CLCalc.R
+++ R/CLCalc.R
@@ -119,7 +119,7 @@ SurvivalPercentage <- function(RAWpath = getwd(), firstDay = 1, measureInterval=
          from OGA(), check that the path passed is correct. If the files are modified
         and the names are changed to exclude results- in the name you can ignore this warning.\n")
   }
-  if(length(grep(pattern=paste0(paste0("*",firstDay),".csv"),file_list))==0) {
+  if(length(grep(pattern=paste0(paste0("*",firstDay),"_.csv"),file_list))==0) {
     print(paste0(paste0("Could not find firstDay csv file, please confirm you have a csv file for that day. Format: <experiment>_Day_",firstDay),".csv"))
     return(-1)
   }
